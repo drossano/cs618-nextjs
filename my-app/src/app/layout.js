4410d3bae1248bfc436a2e7befa661b89379a45c
drossano <deanrossano+dev@gmail.com>
2025-11-06 17:58:10 -0500

    add links between pages

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,11 +1,18 @@
+import { Navigation } from '@/components/Navigation'
+
 export const metadata = {
   title: 'Full-Stack Next.js Blog',
   description: 'A blog about React and Next.js',
 }
 export default function RootLayout({ children }) {
+  const user = { username: 'dean' }
   return (
     <html lang='en'>
       <body>
+        <nav>
+          <Navigation username={user?.username} />
+        </nav>
+        <br />
         <main>{children}</main>
       </body>
     </html>
