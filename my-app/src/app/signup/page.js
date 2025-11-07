c4d89edbb6b892d55e3dfedff09b095f27c8177f
drossano <deanrossano+dev@gmail.com>
2025-11-06 19:34:17 -0500

    implement signing up for an account

--- my-app/src/app/signup/page.js
+++ my-app/src/app/signup/page.js
@@ -1,5 +1,22 @@
-import { second, Signup } from '@/components/Signup'
+import { redirect } from 'next/navigation'
+import { createUser } from '@/data/users'
+import { initDatabase } from '@/db/init'
+import { Signup } from '@/components/Signup'
+
+async function signupAction(prevState, formData) {
+  'use server'
+  try {
+    await initDatabase()
+    await createUser({
+      username: formData.get('username'),
+      password: formData.get('password'),
+    })
+  } catch (err) {
+    return { error: err.message }
+  }
+  redirect('/login')
+}
 
 export default function SignupPage() {
-  return <Signup />
+  return <Signup signupAction={signupAction} />
 }
