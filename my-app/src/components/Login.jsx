6f3e7e3b70260adc37346426a59b5af1a3203afc
drossano <deanrossano+dev@gmail.com>
2025-11-07 12:25:50 -0500

    add logging in

--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -1,17 +1,27 @@
-export function Login() {
-  return(
-    <form >
+'use client'
+import { useFormState } from 'react-dom'
+import PropTypes from 'prop-types'
+
+export function Login({ loginAction }) {
+  const [state, formaAction] = useFormState(loginAction, {})
+  return (
+    <form action={formaAction}>
       <div>
-        <label htmlFor="username">Username:</label>
-        <input type="text" name="username" id="username" />
+        <label htmlFor='username'>Username:</label>
+        <input type='text' name='username' id='username' />
       </div>
       <br />
       <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' name='password' id='password' />
       </div>
       <br />
-      <input type="submit" value="Log In" />
+      <input type='submit' value='Log In' />
+      {state.error ? <strong> Error logging in: {state.error}</strong> : null}
     </form>
   )
+}
+
+Login.propTypes = {
+  loginAction: PropTypes.func.isRequired,
 }
