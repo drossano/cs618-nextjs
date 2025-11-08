1da515fc5d77736d72868251b9b082db2302f116
drossano <deanrossano+dev@gmail.com>
2025-11-08 13:55:10 -0500

    make fetch posts api dynamic

--- my-app/src/app/api/v1/posts/route.js
+++ my-app/src/app/api/v1/posts/route.js
@@ -1,8 +1,9 @@
 import { initDatabase } from '@/db/init'
 import { listAllPosts } from '@/data/posts'
 
+export const dynamic = 'force-dynamic'
 export async function GET() {
   await initDatabase()
   const posts = await listAllPosts()
-  return Response.json({ posts })
+  return Response.json({ posts, currentTime: Date.now() })
 }
