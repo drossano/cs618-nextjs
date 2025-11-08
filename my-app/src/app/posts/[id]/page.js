0a216e1773d20c98d6ffc06371e27ca17c0ad8c2
drossano <deanrossano+dev@gmail.com>
2025-11-08 16:01:05 -0500

    add dyunamic metadata to posts

--- my-app/src/app/posts/[id]/page.js
+++ my-app/src/app/posts/[id]/page.js
@@ -3,6 +3,15 @@ import { notFound } from 'next/navigation'
 import { getPostById } from '@/data/posts'
 import { initDatabase } from '@/db/init'
 
+export async function generateMetadata({ params }) {
+  const id = params.id
+  const post = await getPostById(id)
+  if (!post) notFound()
+  return {
+    title: `${post.title} | Full-Stack Next.js Blog`,
+    description: `Written by ${post.author.username}`,
+  }
+}
 export default async function ViewPostPage({ params }) {
   await initDatabase()
   const post = await getPostById(params.id)
